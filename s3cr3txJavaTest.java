555457edfc114030048c582df097ba8fc6524adc
Gratitech <123098943+GratitechInc@users.noreply.github.com>
2023-02-03 18:05:15 -0600

    Update s3cr3txJavaTest.java

--- s3cr3txJavaTest.java
+++ s3cr3txJavaTest.java
@@ -8,9 +8,9 @@ public class s3cr3txJavaTest {
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.setRequestProperty("Accept", "text/plain");
-        conn.setRequestProperty("Email", "pk@gratitech.com");
-        conn.setRequestProperty("APIToken", "bCrCgRrFoMKyw4PDkyoYF8O9PSXDrsKQRuKAueKAmTbDqsOdacOkw6/Dn8Kzw5/DvRTDjuKAusOyWG3DgG1oUMOBOcObfcOLDwpiwo06CcK2W8OHY3vDmkQzwqTCs1Mzwr/Cvg==");
-        conn.setRequestProperty("AuthCode", "w4zCtm8fH1o7wr/DuB3FoeKAmDtfw5ZvwrLihKLigLA9XcOeMcKtHRYBy4Z5GhTDuMWgwqZoUsOnXR4hCsuG4oChV1HigLDDgMK4w6HFoTbDhlksw7gowrXDhsOMUMOKw4BmFg==");
+        conn.setRequestProperty("Email", "youremail@yourdomain.com");
+        conn.setRequestProperty("APIToken", "YourS3cr3txAPIToken");
+        conn.setRequestProperty("AuthCode", "Yours3cr3txAuthCode");
         conn.setRequestProperty("Input", "This is some s3cr3t Text.");
         conn.setRequestProperty("EorD", "e");
 
@@ -34,9 +34,9 @@ public class s3cr3txJavaTest {
         HttpsURLConnection conn2 = (HttpsURLConnection) url.openConnection();
         conn2.setRequestMethod("GET");
         conn2.setRequestProperty("Accept", "text/plain");
-        conn2.setRequestProperty("Email", "pk@gratitech.com");
-        conn2.setRequestProperty("APIToken", "bCrCgRrFoMKyw4PDkyoYF8O9PSXDrsKQRuKAueKAmTbDqsOdacOkw6/Dn8Kzw5/DvRTDjuKAusOyWG3DgG1oUMOBOcObfcOLDwpiwo06CcK2W8OHY3vDmkQzwqTCs1Mzwr/Cvg==");
-        conn2.setRequestProperty("AuthCode", "w4zCtm8fH1o7wr/DuB3FoeKAmDtfw5ZvwrLihKLigLA9XcOeMcKtHRYBy4Z5GhTDuMWgwqZoUsOnXR4hCsuG4oChV1HigLDDgMK4w6HFoTbDhlksw7gowrXDhsOMUMOKw4BmFg==");
+        conn2.setRequestProperty("Email", "youremail@yourdomain.com");
+        conn2.setRequestProperty("APIToken", "YourS3cr3txAPIToken");
+        conn2.setRequestProperty("AuthCode", "Yours3cr3txAuthCode");
         conn2.setRequestProperty("Input", strResult);
         conn2.setRequestProperty("EorD", "d");
         System.out.println("\nSending 'GET' request to URL: " + website);
